5e02df9ff5b6e67cbba144ef497f590ea38fe4db
Gaurav Ghuge <guduughuge7@gmail.com>
2024-11-15 11:12:21 +0530

    update data type

---
+++ day_1_intro/introDemo.java
@@ -0,0 +1,12 @@
+
+package intro;
+
+
+class introDemo {
+
+   public static void main(String[] args)  {
+
+
+   }
+
+}
---
+++ day_2_dataTypes/dataDemo.java
@@ -0,0 +1,47 @@
+
+package day_2_dataTypes;
+
+class dataDemo {
+
+   public static void main(String args[]) {
+
+      byte tea = 10;
+
+      System.out.println(tea);
+
+      short shirtPrice = 4000;
+
+      System.out.println(shirtPrice);
+
+
+      int samsung_ultra = 68000;
+
+      System.out.println(samsung_ultra);
+
+
+      long lambor_ghini = 214789786;
+
+      System.out.println(lambor_ghini);
+
+
+      float penPrice = 23.78f;
+
+      System.out.println(penPrice);
+
+
+      double stockPrice = 2536428.02734f;
+
+      System.out.println(stockPrice);
+
+
+      char atRate = '@';
+
+      System.out.println(atRate);
+
+
+      boolean visits = false;
+
+      System.out.println(visits);
+
+   }
+}
